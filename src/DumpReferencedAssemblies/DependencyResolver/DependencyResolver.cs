a47e28a10a79447b5a6ca224957ebe8e9727acdc
moerwald <andreas@mewald.com>
2019-12-05 15:00:45 +0100

    Initial commit

---
+++ src/DumpReferencedAssemblies/DependencyResolver/DependencyResolver.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+
+namespace DumpReferencedAssemblies.DependencyResolver
+{
+    public class DependencyResolver
+    {
+        List<string> assemblies = new List<string>();
+
+        public List<string> ResolvedAssemblies { get => new List<string>(assemblies); }
+
+        public void Resolve(string path)
+        {
+            if (string.IsNullOrEmpty(path) || string.IsNullOrWhiteSpace(path))
+            {
+                return;
+            }
+
+            if (assemblies.Contains(path))
+            {
+                // Break the recursion
+                return;
+            }
+
+            try
+            {
+                var asm = Load(path);
+                if (asm != null)
+                {
+                    assemblies.Add(asm.FullName);
+                }
+
+                foreach (var referencedAssemblies in asm.GetReferencedAssemblies())
+                {
+                    Resolve(referencedAssemblies.FullName);
+                }
+            }
+            catch (FileLoadException e)
+            {
+                Console.WriteLine(e);
+            }
+
+        }
+
+        private static Assembly Load(string path)
+        {
+            Assembly asm;
+            if (path.Contains("/"))
+            {
+                asm = Assembly.LoadFrom(path);
+            }
+            else
+            {
+                asm = Assembly.Load(path);
+            }
+
+            return asm;
+        }
+    }
+}
---
+++ src/DumpReferencedAssemblies/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DumpReferencedAssemblies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var resolver = new DependencyResolver.DependencyResolver();
+            resolver.Resolve(args[1]);
+            foreach(var ass in resolver.ResolvedAssemblies)
+            {
+                Console.WriteLine(ass);
+            }
+        }
+    }
+}
